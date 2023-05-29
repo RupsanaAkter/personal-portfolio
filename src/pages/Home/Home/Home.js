383c4f42aa76a4228cce6c1916ccf107c790d1e2
R7558 <rupsanaworld1004@gmail.co>
2023-05-29 21:21:05 +0600

    added footer

---
+++ src/pages/Home/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Introduice from '../Introduice/Introduice';
+
+const Home = () => {
+    return (
+        <div>
+            <Introduice></Introduice>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/pages/Home/Introduice/Introduice.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Introduice = () => {
+    return (
+        <div>
+            <h1>This is Introduice</h1>
+        </div>
+    );
+};
+
+export default Introduice;
