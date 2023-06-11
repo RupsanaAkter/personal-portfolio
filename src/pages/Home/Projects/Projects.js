0a51c63392c8b70c30536e256c3eebfe75322612
R7558 <rupsanaworld1004@gmail.co>
2023-06-11 15:42:15 +0600

    added update all

--- src/pages/Home/Projects/Projects.js
+++ src/pages/Home/Projects/Projects.js
@@ -24,27 +24,27 @@ const Projects = () => {
           className="portfolio-slider"
         >
           <SwiperSlide>
-            <h1 className='text-center font-bold text-2xl'>Resturant</h1>
+            <h1 className='text-2xl font-bold text-center'>Resturant</h1>
             <img src={resturant} alt="" />
-            <h2>Project Details:</h2>
+            <h2 className='text-2xl text-bold'>Project Details:</h2>
             <p>
             
  A nice Resturant page desktop and mobile responsive.
  I found this design in the Figma community.
  I am coding only HTML and CSS for this page without using any CSS or JS library.
-<h3>Technology: HTML, CSS, Javascript</h3>
+<h3 >Technology: HTML, CSS, Javascript</h3>
             </p>
             <div className='text-center'>
-            <button className="btn btn-outline mt-4">
+            <button className="mt-4 btn btn-outline">
             <a href="https://stirring-mochi-9b1367.netlify.app/"  target="_blank">Live site</a>
     </button>
            
                 </div>
           </SwiperSlide>
          <SwiperSlide>
-         <h1 className='text-center font-bold text-2xl'>OpenAI</h1>
+         <h1 className='text-2xl font-bold text-center'>OpenAI</h1>
             <img src={ai} alt="" />
-            <h2>Project Details:</h2>
+            <h2 className='text-2xl text-bold'>Project Details:</h2>
             <p>
             
  A nice OpenAI page desktop and mobile responsive.
@@ -53,16 +53,16 @@ const Projects = () => {
 <h3>Technology: HTML, CSS, React JS</h3>
             </p>
             <div className='text-center'>
-            <button className="btn btn-outline mt-4">
+            <button className="mt-4 btn btn-outline">
             <a href="https://candid-stroopwafel-4ece65.netlify.app/"  target="_blank">Live site</a>
     </button>
            
                 </div>
           </SwiperSlide>
           <SwiperSlide>
-          <h1 className='text-center font-bold text-2xl'>News-portal</h1>
+          <h1 className='text-2xl font-bold text-center'>News-portal</h1>
             <img src={news} alt="" />
-            <h2>Project Details:</h2>
+            <h2 className='text-2xl text-bold'>Project Details:</h2>
             <p>
             
  A nice News-portal page desktop and mobile responsive.
@@ -71,16 +71,16 @@ const Projects = () => {
 <h3>Technology: HTML, CSS, Javascript</h3>
             </p>
             <div className='text-center'>
-            <button className="btn btn-outline mt-4">
+            <button className="mt-4 btn btn-outline">
             <a href="https://64859316ad38cc75d71d3bc5--guileless-frangipane-87ce7f.netlify.app/"  target="_blank">Live site</a>
     </button>
            
                 </div>
           </SwiperSlide>
           <SwiperSlide>
-          <h1 className='text-center font-bold text-2xl'>Hospital</h1>
+          <h1 className='text-2xl font-bold text-center'>Hospital</h1>
             <img src={hospital} alt="" />
-            <h2>Project Details:</h2>
+            <h2 className='text-2xl text-bold'>Project Details:</h2>
             <p>
             
  A nice Hospital page desktop and mobile responsive.
@@ -89,16 +89,16 @@ const Projects = () => {
 <h3>Technology: HTML, CSS, Javascript</h3>
             </p>
             <div className='text-center'>
-            <button className="btn btn-outline mt-4">
+            <button className="mt-4 btn btn-outline">
             <a href="https://648592db2fa55b753425b7eb--resplendent-licorice-ae02e3.netlify.app/"  target="_blank">Live site</a>
     </button>
            
                 </div>
           </SwiperSlide> 
           <SwiperSlide>
-          <h1 className='text-center font-bold text-2xl'>Hosting-company</h1>
+          <h1 className='text-2xl font-bold text-center'>Hosting-company</h1>
             <img src={hosting} alt="" />
-            <h2>Project Details:</h2>
+            <h2 className='text-2xl text-bold'>Project Details:</h2>
             <p>
             
  A nice Hosting-company page desktop and mobile responsive.
@@ -107,7 +107,7 @@ const Projects = () => {
 <h3>Technology: HTML, CSS, Javascript</h3>
             </p>
             <div className='text-center'>
-            <button className="btn btn-outline mt-4">
+            <button className="mt-4 btn btn-outline">
             <a href="https://64859274f1e5e1776ffd99a4--precious-jelly-b4ecf7.netlify.app/"  target="_blank">Live site</a>
     </button>
            
